6847bc614a756bf76eae79b828c7a5bbe20b654c
Jeff <JeffYFHuang@github.com>
2020-05-05 15:41:38 +0800

    remove unnecessary processes

--- k8sdeployment/gpustat_web/run.sh
+++ k8sdeployment/gpustat_web/run.sh
@@ -2,7 +2,7 @@
 # do other things here
 
 # kafka service ip
-echo "10.35.65.28 deepops-desktop" >> /etc/hosts
-python3 gpu_info_insert.py
-python3 metrics_consumer.py &
+#echo "10.35.65.28 deepops-desktop" >> /etc/hosts
+#python3 gpu_info_insert.py
+#python3 metrics_consumer.py &
 python3 -m gpustat_web --port 5555 --exec "gpustat --json" 10.36.48.130 10.36.48.131 2>&1
